60bfe2e43dc14a9c55fafa8f235d7f415602ae57
Dia Raboana <rdiadama@gmail.com>
2025-04-09 23:43:19 -0400

    added arguments management

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    action: String,
+    function: String, 
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    
+    match args.action.as_str() {
+        "get" => println!("You want to get a function ?"),
+        "doc" => println!("You want the doc of a function ?"),
+        _ => println!("You're action doesn't exist."),
+    }
 }
