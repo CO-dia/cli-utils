47f7f05547027460680b86b026c781b1097314da
Dia Raboana <rdiadama@gmail.com>
2025-04-11 22:09:30 -0400

    added find file in cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::fs;
+use std::path::Path;
+use std::io::{self, Read};
 
 #[derive(Parser)]
 struct Cli {
@@ -10,8 +13,29 @@ fn main() {
     let args = Cli::parse();
     
     match args.action.as_str() {
-        "get" => println!("You want to get a function ?"),
+        "get" => access_file(&args.function).unwrap(),
         "doc" => println!("You want the doc of a function ?"),
         _ => println!("You're action doesn't exist."),
     }
+}
+
+fn access_file(filename: &str) -> io::Result<()> {
+    let utils_dir = "../ts-utils";
+    let filepath = format!("{}/{}{}", utils_dir, filename, ".ts");
+    let path = Path::new(&filepath);
+    
+    if !path.exists() {
+        println!("Error: File '{}' does not exist", filepath);
+        return Ok(());
+    }
+    
+    // Read and display the file content
+    let mut file = fs::File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    
+    println!("Contents of {}:", filepath);
+    println!("{}", contents);
+    
+    Ok(())
 }
